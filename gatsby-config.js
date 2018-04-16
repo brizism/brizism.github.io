4233dd090b3e69c0208f47825f2ff237946a1c9a
brizism <montielbriseida@gmail.com>
2018-04-15 23:24:48 -0400

    add google analytics, manifest and sw

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,25 @@ module.exports = {
     title: 'Briseida Montiel',
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass'
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-47952650-1`,
+        head: false,
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Briseida Montiel",
+        short_name: "Briseida's Portfolio",
+        start_url: "/",
+        background_color: "#F6F6F6",
+        icon: "src/img/favicon.png" 
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
