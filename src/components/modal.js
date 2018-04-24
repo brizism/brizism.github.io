36276170cf5716519352fdde4ab4904d55d3735f
brizism <montielbriseida@gmail.com>
2018-04-24 13:46:43 -0400

    files deleted

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Link from 'gatsby-link'
+import resume from '../img/briseida_montiel_resume.pdf'
 
 const displayNone = {
   display: 'none'
@@ -31,7 +32,7 @@ class Modal extends Component {
             <a href="https://www.linkedin.com/in/briseidamontiel/" target="_blank"><i className="fa fa-linkedin"></i></a>
             <a href="https://github.com/brizism" target="_blank"><i className="fa fa-github"></i></a>
             <a href="https://twitter.com/brizism" target="_blank"><i className="fa fa-twitter"></i></a>
-            <Link to="../img/briseida_montiel_resume.pdf" target="_blank"><i className="fa fa-file-text-o"></i></Link>
+            <a href={resume} target="_blank"><i className="fa fa-file-text-o"></i></a>
           </div>
         </div>
       </div>
